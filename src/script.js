d7fea619b0a74b33ac910f1d7abc91e0a0a540b0
GarimaChauhan122 <chauhan19garima@gmail.com>
2025-04-28 23:04:02 +0200

    API Integration

--- src/script.js
+++ src/script.js
@@ -1,7 +1,19 @@
+function refreshDetails(response) {
+  document.querySelector("#city-name-id").innerHTML = response.data.city;
+  let temp = Math.round(response.data.temperature.current);
+  document.querySelector("#temp-id").innerHTML = temp;
+}
+
+function searchCity(city) {
+  let apiKey = "742ecdbd8fa1dfo0tac0da7480944305";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshDetails);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault(); /*ie reloading the page */
   let cityElement = document.querySelector("#search-input-id");
-  document.querySelector("#city-name-id").innerHTML = cityElement.value;
+  searchCity(cityElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form-id");
